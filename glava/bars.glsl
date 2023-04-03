42c606177894cfd95bf73559387d14ebfdffffcf
MagnesiaReal <mgarridor1400@alumno.ipn.mx>
2023-04-03 11:16:18 -0600

    refactor(configs):adapt for two screens

--- glava/bars.glsl
+++ glava/bars.glsl
@@ -8,9 +8,9 @@
 /* Outline color */
 #define BAR_OUTLINE #00000099
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
-#define BAR_OUTLINE_WIDTH 5
+#define BAR_OUTLINE_WIDTH 1
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 500
+#define AMPLIFY 700
 /* Whether the current settings use the alpha channel;
    enabling this is required for alpha to function
    correctly on X11 with `"native"` transparency. */
--- magne-scripts/wacom_config.sh
+++ magne-scripts/wacom_config.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # for left handed
-xsetwacom --set "HUION H420 stylus" rotate half
 # Map to screen
-# available: DVI-0, VGA-0
-xsetwacom --set "HUION H420 stylus" MapToOutput $1
+# available: DVI-0, VGA-0, DisplayPort-0
+xsetwacom --set "HUION H420 Pen stylus" rotate half
+xsetwacom --set "HUION H420 Pen stylus" MapToOutput $1
+xsetwacom --set "HUION H420 Pen stylus" Area 0 3588 1946 4680
 
 xsetwacom --set "HUION H420 Pad pad" Button 1 key "ctrl z"
 xsetwacom --set "HUION H420 Pad pad" Button 2 key "ctrl y"
