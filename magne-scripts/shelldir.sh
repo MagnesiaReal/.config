f00489bd625374633e5eb7871aa7b7c74c9d6222
MagnesiaReal <mgarridor1400@alumno.ipn.mx>
2023-12-09 22:21:53 -0600

    last 2023

--- magne-scripts/shelldir.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-WHEREAMI=$(cat /tmp/whereami)
-kitty -d "$WHEREAMI"
--- magne-scripts/transform_waifu2x.sh
+++
@@ -1,9 +0,0 @@
-IMAGES=`find ./target/`
-
-for file in `ls target/`; do
-  waifu2x-ncnn-vulkan -s 2 -i ./target/$file -o ./size/$file
-done;
-
-for file in `ls target/`; do
-  waifu2x-ncnn-vulkan -n 3 -i ./size/$file -o ./noise/$file
-done;
